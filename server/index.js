8b3c95c5b4095e3a096d17675b9fe9f22fecd0d1
gauravraj0510 <gaurav.rajghs@gmail.com>
2023-07-03 00:21:09 +0530

    server updates

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limig: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
-app.use("/api/v1/dalle", dalleRoutes);
+app.use("/", dalleRoutes);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from ClothifyAI" });
-});
+// app.get("/test", (req, res) => {
+//   res.status(200).json({ message: "Hello from ClothifyAI" });
+// });
 
 app.listen(8080, () => console.log("Server has started on port 8080"));
